1c0dc987b1a962fa081f0bae02667a9854b8b125
ChristinaWu0401 <cc231075@fhstp.ac.at>
2023-12-21 14:44:22 +0100

    Add direction input logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+type Direction = "up" | "right" | "left" | "down";
 const backgroundColor = "#285425";
 const snakeColor = "red";
 const pixelsPerTile = 20;
@@ -7,10 +8,30 @@ function drawSnake(ctx: CanvasRenderingContext2D) {
   ctx.fillRect(0, 0, pixelsPerTile, pixelsPerTile);
 }
 
+function onDirectionPress(direction: Direction) {
+}
+
 window.onload = function () {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d")!;
   canvas.style.backgroundColor = backgroundColor;
 
   drawSnake(ctx);
+
+  window.onkeyup = function (keyboardEvent) {
+    switch (keyboardEvent.key) {
+      case "w":
+        onDirectionPress("up");
+        break;
+      case "s":
+        onDirectionPress("down");
+        break;
+      case "a":
+        onDirectionPress("left");
+        break;
+      case "d":
+        onDirectionPress("right");
+        break;
+    }
+  };
 };
