73fb56c73b3df780ee8949f593655e38e7d7c2f3
ChristinaWu0401 <cc231075@fhstp.ac.at>
2023-12-21 14:19:24 +0100

    Add basic draw snake logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 const backgroundColor = "#285425";
+const snakeColor = "red";
+const pixelsPerTile = 20;
+
+function drawSnake(ctx: CanvasRenderingContext2D) {
+  ctx.fillStyle = snakeColor;
+  ctx.fillRect(0, 0, pixelsPerTile, pixelsPerTile);
+}
+
 window.onload = function () {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d")!;
   canvas.style.backgroundColor = backgroundColor;
+
+  drawSnake(ctx);
 };
