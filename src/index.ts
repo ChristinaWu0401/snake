cc5c1a76a0eef4ed7f629c9cdcec9e6614002db3
ChristinaWu0401 <cc231075@fhstp.ac.at>
2023-12-21 13:54:42 +0100

    Add background color

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 console.log("Hello world!");
+const backgroundColor = "#285425";
 window.onload = function () {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d")!;
+  canvas.style.backgroundColor = backgroundColor;
 };
